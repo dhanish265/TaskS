41130fcfbeae36917f53f9c0017f3d7017160b51
dhanish265 <dhanish265@gmail.com>
2022-09-16 17:29:04 +0800

    commit

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -15,8 +15,13 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
+
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//        printDeadlinesUsingStream(tasksData);
+
+//        printData(tasksData);
+//        printDataUsingStream(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
         System.out.println("Total number of deadlines (using stream): " + countDeadlinesUsingStream(tasksData));
