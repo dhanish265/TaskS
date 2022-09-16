a6b4960af254efabf7ffd9abd4d1d005bc8a6b6c
dhanish265 <dhanish265@gmail.com>
2022-09-16 17:25:23 +0800

    add code to print deadlines using stream

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -32,17 +32,40 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+
+        int count = (int) tasks.stream()
+                        .filter(t -> t instanceof Deadline)
+                        .count();
+        return count;
+    }
+
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data using loop");
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    public static void printDataUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing data using streams");
+        tasks.stream()
+                .forEach(System.out::println);
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using loop");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing deadline using streams");
+        tasks.parallelStream()
+                .filter(t -> t instanceof Deadline)
+                .forEach(System.out::println);
     }
 }
